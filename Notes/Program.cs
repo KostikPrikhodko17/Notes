66f31bbdb9258d9029584f1b26b1b34b442c6267
kostik <kostik.prikhodko2007@gmail.com>
2026-08-03 20:30:43 +0300

    Добавлена функция просмотра заметок

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -3,29 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Diagnostics;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
 
 namespace Notes
 {
     class Program
     {
+        const string FILE_PATH = "notes.txt";
         static void Main()
         {
             ModelNote modelNote = new ModelNote();
             List<ModelNote> notes = new List<ModelNote>();
 
-            Console.WriteLine("\n1 - Создать заметку");
-            string input = Console.ReadLine();
-
-            switch (input)
+            if (File.Exists(FILE_PATH))
             {
-                case "1":
-                    CreateNote(notes);
-                    break;
-
-                default:
-                    Console.WriteLine("\nНекоректный ввод!");
-                    break;
+                string data = File.ReadAllText(FILE_PATH);
+                notes = JsonSerializer.Deserialize<List<ModelNote>>(data) ?? new List<ModelNote>();
             }
+
+            bool isRunning = true;
+
+            do
+            {
+                Console.WriteLine("\n1 - Создать заметку\n\n2 -Просмотреть список заметок\n\n3 - Посмотреть содержимое заметки\n\n4 - Сохранить и выйти");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        CreateNote(notes);
+                        break;
+
+                    case "2":
+                        if (!notes.Any())
+                        {
+                            NotesIsEmpty();
+                            break;
+                        }
+                        ShowListNotes(notes);
+                        break;
+
+                    case "3":
+                        if (!notes.Any())
+                        {
+                            NotesIsEmpty();
+                            break;
+                        }
+                        DisplayNoteContent(notes);
+                        break;
+
+                    case "4":
+                        SaveToData(notes);
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("\nНекоректный ввод!");
+                        break;
+                }
+            }
+            while (isRunning);
+
+
+        }
+
+        static void NotesIsEmpty()
+        {
+            Console.WriteLine("\nСписок заметок пуст!");
         }
 
         static void CreateNote(List<ModelNote> notes)
@@ -69,6 +116,66 @@ namespace Notes
                 {
                     Console.WriteLine("\nНекоректный ввод!");
                 }
+            }
+        }
+
+        static void ShowListNotes(List<ModelNote> notes) 
+        {
+            Console.WriteLine($"{"ID", -5} {"Заголовок", -20} {"Дата", -30}");
+            Console.WriteLine(new string('-', 70));
+            foreach (var note in notes)
+            {
+                Console.WriteLine($"\n{note.Id, -5} {note.Title, -20} {note.Date.ToString("dd.MM.yyyy"), -30}");
+            }
+        }
+
+        static void DisplayNoteContent(List<ModelNote> notes)
+        {
+            ShowListNotes(notes);
+
+            Console.WriteLine("\nВернуться в меню - x\nВведите ID заметки\n");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int noteId))
+            {
+                var note = notes.FirstOrDefault(n => n.Id == noteId);
+
+                if (note != null)
+                {
+                    Console.WriteLine($"\n{note.Title}\n{note.Content}");
+                }
+                else
+                {
+                    Console.WriteLine("\nЗаметка с таким ID не найдена!");
+                }
+            }
+            else if (input.ToLower() == "x")
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("\nНекоректный ввод!");
+            }
+        }
+
+
+
+        static void SaveToData(List<ModelNote> notes)
+        {
+            var options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                WriteIndented = true
+            };
+            try
+            {
+                string data = JsonSerializer.Serialize(notes, options);
+                File.WriteAllText(FILE_PATH, data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при сохранении данных: {ex.Message}");
             }
         }
     }
